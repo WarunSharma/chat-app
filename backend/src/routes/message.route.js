f1bbc80ca329fc4741f2ea2a19fa3e14f18ae499
WarunSharma <warunsharma.95@gmail.com>
2025-08-02 18:12:37 +0530

    add: sidebar component

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,7 +4,7 @@ import { getMessages, getUsersForSidebar } from '../controllers/user.controller.
 
 const router = express.Router();
 
-router.get('/User', protectRoute, getUsersForSidebar);
+router.get('/users', protectRoute, getUsersForSidebar);
 router.get('/:id', protectRoute, getMessages);
 
 export default router;
